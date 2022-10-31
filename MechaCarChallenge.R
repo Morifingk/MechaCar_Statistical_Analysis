f8ccdff5e85a64e903f8805ec32e4a59b55cbd91
Morifingk <104086409+Morifingk@users.noreply.github.com>
2022-10-31 17:33:30 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,24 @@
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("ggplot2")
+read.csv(file.choose())
+#reg <- lm("vehicle_length","vehicle_weight","spoiler_angle","ground_clearance","AWD","mpg"~ bp, data=MechaCar_mpg)
+lm(formula =  vehicle_weight ~ vehicle_length, data = MechaCar_mpg)
+lm(formula = ground_clearance ~ spoiler_angle, data = MechaCar_mpg)
+lm(formula = mpg ~ AWD, data = MechaCar_mpg)
+summary(MechaCar_mpg)
+ 
+#read.csv(file.choose())
+suspension_data <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+ 
+total_summary <- suspension_data %>% summarize(Mean_PSI=mean(PSI),
+                                                Median_PSI=median(PSI),
+                                                Var_PSI=var(PSI),
+                                                Std_Dev_PSI=sd(PSI),
+                                                Num_Coil=n(), .groups = 'keep')
+lot_summary <- suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                      Median_PSI=median(PSI),
+                                                                      Var_PSI=var(PSI),
+                                                                      Std_Dev_PSI=sd(PSI),
+                                                                      Num_Coil=n(), .groups = 'keep')
+
